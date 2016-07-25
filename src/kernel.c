d62146931da5713eaab2828af24c9fa869508d84
Sebastian Green-Husted <ricecake@tfm.nu>
2016-07-24 21:29:51 -0400

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,4 @@
-/* Surely you will remove the processor conditionals and this comment
-   appropriately depending on whether or not you use C++. */
-#if !defined(__cplusplus)
-#include <stdbool.h> /* C doesn't have booleans by default. */
-#endif
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
